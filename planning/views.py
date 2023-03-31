90edea7a43e78a195e60442b7c17c58387f1da74
Muhammed Ajmal <mohammedajmal996@gmail.com>
2023-03-31 23:46:05 +0530

    added endpoint to list data

--- planning/views.py
+++ planning/views.py
@@ -9,20 +9,20 @@ from .models import Bom, MaterialList, Product
 @api_view(['get'])
 def list_bom(request):
     list_=Bom.objects.all();
-    B=dict(BomSerializer(list_,many=True).data)
+    B=BomSerializer(list_,many=True).data
     
     return JsonResponse(B,safe=False)
 
 @api_view(['get'])
 def list_product(request):
     list_=Product.objects.all();
-    B=dict(Product_Serializer(list_,many=True).data)
+    B=Product_Serializer(list_,many=True).data
     
     return JsonResponse(B,safe=False)
 
 @api_view(['get'])
 def list_materials(request):
     list_=MaterialList.objects.all();
-    B=dict(MaterialSerializer(list_,many=True).data)
+    B=MaterialSerializer(list_,many=True).data
     
     return JsonResponse(B,safe=False)
