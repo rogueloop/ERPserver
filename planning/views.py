c24dc59e12e1aa59de82b021981bc20281ce2ff6
Muhammed Ajmal <mohammedajmal996@gmail.com>
2023-04-15 16:26:14 +0530

    added put function in the product api

--- planning/views.py
+++ planning/views.py
@@ -45,6 +45,19 @@ class Product_API_View(APIView):
             return Response({'success': serializer.data}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+    def put(self,request,pk,*args):
+        try:
+            product=Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({'message':str(Product.DoesNotExist)},status=status.HTTP_404_NOT_FOUND)
+        serializer=self.serializer_class(product,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
 class Bom_API_View(APIView):
