b8f799ccf97eb5db790489584cdafafd1320c14b
Sreedeep CV <sreedeep200@gmail.com>
2023-05-06 20:15:38 +0530

    Added notifications Model

--- planning/models.py
+++ planning/models.py
@@ -344,3 +344,17 @@ class Prdetaillog(models.Model):
         managed = True
         db_table = 'prdetaillog'
         unique_together = (('slno', 'srlno'),)
+
+
+class Notification(models.Model):
+    message = models.CharField(max_length=255,blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # def __str__(self):
+    #     return f"{self.user.username}: {self.message}"
+    
+    class Meta:
+        managed = True
+        db_table = 'notification'
+
