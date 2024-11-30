5f506f76bee86dcd8f8a6a51af8b0d6639eb23da
Sreedeep CV <sreedeep200@gmail.com>
2024-11-30 20:22:11 +0530

    test commit

--- planning/urls.py
+++ planning/urls.py
@@ -9,7 +9,7 @@ urlpatterns=[
   path('Product_api/<str:pk>/',Product_API_View.as_view(),name='Product_get_by_id'),
   path('Material_api/<int:pk>/', Material_API.as_view(), name='Material_api'),
   path('Material_api/', Material_API.as_view(), name='Material_get_By_id'),
-  path('Bom_api/<str:pk>/',Bom_API_View.as_view(),name='Bom_api'),
+  path('Bom_api/v1/<str:pk>/',Bom_API_View.as_view(),name='Bom_api'),
   path('Bom_api/<str:product_id>', Bom_API_View.as_view(), name='Bom_api'),
   path('Stock_api/', AddStockAPI.as_view(), name='Stock_api'),
   path('notify_limit/',NotifyLimitAPI.as_view(), name='notify_limit'),
@@ -21,7 +21,5 @@ urlpatterns=[
   path('status/<int:pk>', Status_Api.as_view()),
   path('material_suggestion',material_suggestion)
 
-  
- 
 
 ]
